674411f966f05478b3fb1c26ce166d0474cc8ff3
WetPirat1 <EvgenSemydosky2@gmail.com>
2024-05-30 13:48:57 +0300

    sceleton of a design

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+const gulp = require('gulp')
+const sass = require('gulp-sass')(require('sass'));
+const del = require('del')
+
+const paths = {
+    styles: {
+        src:['src/styles/**/*.scss','src/styles/**/*.sass'],
+        dest: 'dist/styles'
+    },
+    scripts: {
+        src: 'src/scripts/**/*.js',
+        dest: 'dist/js/'
+    }
+}
+
+function clean() {
+    return del(['dist'])
+}
+
+function styles() {
+    return gulp.src(paths.styles.src)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest(paths.styles.dest))
+}
+
+function watch () {
+    gulp.watch(paths.styles.src, styles)
+}
+const build = gulp.series(clean, styles, watch)
+
+
+exports.clean = clean
+exports.styles = styles
+exports.watch = watch
+exports.build = build
+exports.default = build
